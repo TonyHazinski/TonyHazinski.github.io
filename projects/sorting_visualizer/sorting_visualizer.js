aadbae20732cadfecf196f527861dff009cd2715
Tony Hazinski <hazaj2000@gmail.com>
2022-12-23 14:47:29 -0600

    Update

--- projects/sorting_visualizer/sorting_visualizer.js
+++ projects/sorting_visualizer/sorting_visualizer.js
@@ -1,9 +1,28 @@
-function start_visualization() {
-    let unsorted_array = [];
-    let display_array = document.getElementById("array")
+let unsortedArray = [];
+let displayArray = document.getElementById("array");
+
+function startVisualization() {
     for (let i = 0; i < 10; i++) {
         let num = Math.floor(Math.random()*100);
-        unsorted_array.push(num);
+        unsortedArray[i] = num;
         document.getElementById(i).innerText = num;
+    }
+
+    let selectedSort = document.getElementById("sort-select").value;
+
+    if(selectedSort === "bubble") {
+        visualizeBubbleSort();
+    }
+}
+
+function visualizeBubbleSort() {
+    for(let i = 0; i < unsortedArray.length - 1; i++) {
+        for (let j = 0; j < unsortedArray.length - 1; j++) {
+            if(unsortedArray[j] > unsortedArray[j + 1]) {
+                let temp = unsortedArray[j];
+                unsortedArray[j] = unsortedArray[j + 1];
+                unsortedArray[j + 1] = temp;
+            }
+        }
     }
 }
