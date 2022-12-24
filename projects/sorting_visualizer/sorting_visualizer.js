7efdc87f2a8be3a248baf56d4f512e127aec1098
Tony Hazinski <hazaj2000@gmail.com>
2022-12-24 13:20:27 -0600

    Switched from for loop to recursion to make the code syncronous.

--- projects/sorting_visualizer/sorting_visualizer.js
+++ projects/sorting_visualizer/sorting_visualizer.js
@@ -1,12 +1,64 @@
 let unsortedArray = [];
-let displayArray = document.getElementById("array");
 
-function startVisualization() {
+function randomizeArray() {
     for (let i = 0; i < 10; i++) {
         let num = Math.floor(Math.random()*100);
         unsortedArray[i] = num;
         document.getElementById(i).innerText = num;
     }
+}
+
+function incrementI(i) {
+    if (i < unsortedArray.length - 1) {
+        let j = 0;
+        incrementJ(i, j)
+    }
+}
+
+function incrementJ(i, j) {
+    console.log("i = " + i)
+    console.log("j = " + j)
+    let animated = true;
+    if(unsortedArray[j] > unsortedArray[j + 1]) {
+        animated = false;
+        let temp = unsortedArray[j];
+        unsortedArray[j] = unsortedArray[j + 1];
+        unsortedArray[j + 1] = temp;
+        let element = document.getElementById(j)
+        let second = document.getElementById(j + 1)
+        element.style.top = "0px"
+        element.style.left = "0px"
+        second.style.left = "0px"
+        let moveDown = setInterval(() => {
+            if (element.style.top.slice(0, -2) < 70 && element.style.left.slice(0, -2) < 62) {
+                element.style.top = parseInt(element.style.top.slice(0, -2)) + 1 + "px";
+            } 
+            else if (second.style.left.slice(0, -2) > -62 && element.style.left.slice(0, -2) < 62) {
+                second.style.left = second.style.left.slice(0, -2) - 1 + "px";
+                element.style.left = parseInt(element.style.left.slice(0, -2)) + 1 + "px";
+            } else if (element.style.top.slice(0, -2) > 0) {
+                element.style.top = parseInt(element.style.top.slice(0, -2)) - 1 + "px";
+            }
+            else {
+                element.innerText = unsortedArray[j];
+                second.innerText = unsortedArray[j + 1];
+                element.style.top = "0px"
+                element.style.left = "0px"
+                second.style.left = "0px"
+                clearInterval(moveDown)
+                incrementJ(i, j + 1)
+            }
+        }, 500/70);
+    }
+    if (j < unsortedArray.length -1 && animated) {
+        incrementJ(i, j + 1)
+    }
+    else if (animated){
+        incrementI(i + 1)
+    }
+}
+
+function startVisualization() {
 
     let selectedSort = document.getElementById("sort-select").value;
 
@@ -16,13 +68,38 @@ function startVisualization() {
 }
 
 function visualizeBubbleSort() {
-    for(let i = 0; i < unsortedArray.length - 1; i++) {
-        for (let j = 0; j < unsortedArray.length - 1; j++) {
-            if(unsortedArray[j] > unsortedArray[j + 1]) {
-                let temp = unsortedArray[j];
-                unsortedArray[j] = unsortedArray[j + 1];
-                unsortedArray[j + 1] = temp;
-            }
-        }
-    }
+    incrementI(0)
+    // for(let i = 0; i < unsortedArray.length - 1; i++) {
+    //     for (let j = 0; j < unsortedArray.length - 1; j++) {
+    //         if(unsortedArray[j] > unsortedArray[j + 1]) {
+    //             let temp = unsortedArray[j];
+    //             unsortedArray[j] = unsortedArray[j + 1];
+    //             unsortedArray[j + 1] = temp;
+    //             let element = document.getElementById(j)
+    //             let second = document.getElementById(j + 1)
+    //             element.style.top = "0px"
+    //             element.style.left = "0px"
+    //             second.style.left = "0px"
+    //             let moveDown = setInterval(() => {
+    //                 if (element.style.top.slice(0, -2) < 70 && element.style.left.slice(0, -2) < 62) {
+    //                     element.style.top = parseInt(element.style.top.slice(0, -2)) + 1 + "px";
+    //                 } 
+    //                 else if (second.style.left.slice(0, -2) > -62 && element.style.left.slice(0, -2) < 62) {
+    //                     second.style.left = second.style.left.slice(0, -2) - 1 + "px";
+    //                     element.style.left = parseInt(element.style.left.slice(0, -2)) + 1 + "px";
+    //                 } else if (element.style.top.slice(0, -2) > 0) {
+    //                     element.style.top = parseInt(element.style.top.slice(0, -2)) - 1 + "px";
+    //                 }
+    //                 else {
+    //                     element.innerText = unsortedArray[j];
+    //                     second.innerText = unsortedArray[j + 1];
+    //                     element.style.top = "0px"
+    //                     element.style.left = "0px"
+    //                     second.style.left = "0px"
+    //                     clearInterval(moveDown)
+    //                 }
+    //             }, 1000/70);
+    //         }
+    //     }
+    // }
 }
